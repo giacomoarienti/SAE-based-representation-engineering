7ec2607a8717610e00aa9c59fe3f88426d3e8969
giacomoarienti <giacomo.arienti2@studio.unibo.it>
2025-03-14 18:51:39 +0100

    Change attention implementation in init_frozen_language_model to 'sdpa'

--- spare/utils.py
+++ spare/utils.py
@@ -55,7 +55,7 @@ def load_model(model_path, flash_attn, not_return_model=False):
     return model, tokenizer
 
 
-def init_frozen_language_model(model_path, attn_imp="eager"):
+def init_frozen_language_model(model_path, attn_imp="sdpa"):
     bf16 = torch.bfloat16
     if "llama" in model_path.lower():
         model = LlamaForCausalLM.from_pretrained(model_path, attn_implementation=attn_imp, torch_dtype=bf16, device_map="cuda")
