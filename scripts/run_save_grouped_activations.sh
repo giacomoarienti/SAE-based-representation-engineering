6462fa1e826cc55df59841ef30fc65f36c702179
giacomoarienti <giacomo.arienti2@studio.unibo.it>
2025-03-18 14:58:49 +0100

    default grouped_activation with 3 shots

--- scripts/run_save_grouped_activations.sh
+++ scripts/run_save_grouped_activations.sh
@@ -12,8 +12,7 @@ python -m spare.save_grouped_activations \
   --data_name="nqswap" \
   --model_path=${MODEL_PATH} \
   --load_data_name="grouped_prompts"\
-  --shots_to_encode 3 4 5 \
-#  --seeds_to_encode 42 43 44 45 46 \
+  --shots_to_encode 3 \
   --seeds_to_encode 42 43 \
   --save_hiddens_name="grouped_activations"
 
