05211557887894d0fd68f2edab31c24ca72db15f
giacomoarienti <giacomo.arienti2@studio.unibo.it>
2025-03-19 15:25:49 +0100

    Add support for Llama-3.2-1B model and improve result calculations

--- scripts/avg_acc.py
+++ scripts/avg_acc.py
@@ -38,6 +38,11 @@ def get_results(model_name, data_name):
             f"gemma-2-9b-{data_name}-23,24,25,29,30,31-0.01-1.8-grouped_activations-mutual_information-True-True-45",
             f"gemma-2-9b-{data_name}-23,24,25,29,30,31-0.01-1.8-grouped_activations-mutual_information-True-True-46",
         ]
+    elif model_name == "Llama-3.2-1B":
+        use_context_files = [
+            f"Llama-3.2-1B-{data_name}-10-0.07-2.0-grouped_activations_3shot_seeds42-43-multiprocess-mutual_information-grouped_activations_3shot_seeds42-43-False-True-42",
+        ]
+        use_parameter_files= []
     else:
         raise ValueError
 
@@ -53,25 +58,27 @@ def get_results(model_name, data_name):
 
     use_context_em = []
     for cur_result in use_context_results:
-        use_context_em.append(sum(cur_result["all_sub_scores"]) / len(cur_result["all_sub_scores"]) * 100)
+        use_context_em.append(sum(cur_result["use_context_sub_scores"]) / len(cur_result["use_context_sub_scores"]) * 100)
     use_parameter_em = []
     for cur_result in use_parameter_results:
-        use_parameter_em.append(sum(cur_result["all_org_scores"]) / len(cur_result["all_org_scores"]) * 100)
+        use_parameter_em.append(sum(cur_result["use_parameter_org_scores"]) / len(cur_result["use_parameter_org_scores"]) * 100)
 
-    print("use_context_em")
-    print(use_context_em)
-    print(f"avg: {np.mean(use_context_em):.2f}")
-    print(f"std: {np.std(use_context_em):.2f}")
+    if use_context_em:
+        print("use_context_em")
+        print(use_context_em)
+        print(f"avg: {np.mean(use_context_em):.2f}")
+        print(f"std: {np.std(use_context_em):.2f}")
 
-    print("use_parameter_em")
-    print(use_parameter_em)
-    print(f"avg: {np.mean(use_parameter_em):.2f}")
-    print(f"std: {np.std(use_parameter_em):.2f}")
+    if use_parameter_em:
+        print("use_parameter_em")
+        print(use_parameter_em)
+        print(f"avg: {np.mean(use_parameter_em):.2f}")
+        print(f"std: {np.std(use_parameter_em):.2f}")
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_path", type=str, required=True)
+    parser.add_argument("--model_name", type=str, required=True)
     parser.add_argument("--data_name", type=str, required=True)
     args = parser.parse_args()
     get_results(args.model_name, args.data_name)
