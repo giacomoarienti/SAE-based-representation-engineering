c9ab4f281b288d383d5852996a7de64388c2a949
giacomoarienti <arientigiacomo2003@gmail.com>
2025-03-22 21:07:32 +0100

    run_group_prompt

--- scripts/run_group_prompts.sh
+++ scripts/run_group_prompts.sh
@@ -3,24 +3,29 @@
 set -e
 set -u
 
-export CUDA_VISIBLE_DEVICES=0
+if [ $# -lt 1 ]; then
+  echo "Error: Model path not provided."
+  echo "Usage: $0 <model_path>"
+  exit 1
+fi
 
+MODEL_PATH="${1}"
 SAVE_DIR_NAME="grouped_prompts"  # save to PROJ_DIR / "cache_data" / model_name / SAVE_DIR_NAME
 
-python -m spare.group_prompts \
+python3 -m spare.group_prompts \
   --model_path=${MODEL_PATH} \
   --save_dir_name=${SAVE_DIR_NAME} \
   --k_shot=3 \
-  --seeds_to_encode 42 43 #44 45 46
+  --seeds_to_encode 42 43 44 45 46
 
-# python -m spare.group_prompts \
-#   --model_path=${MODEL_PATH} \
-#   --save_dir_name=${SAVE_DIR_NAME} \
-#   --k_shot=4 \
-#   --seeds_to_encode 42 43 44 45 46
+python3 -m spare.group_prompts \
+  --model_path=${MODEL_PATH} \
+  --save_dir_name=${SAVE_DIR_NAME} \
+  --k_shot=4 \
+  --seeds_to_encode 42 43 44 45 46
 
-# python -m spare.group_prompts \
-#   --model_path=${MODEL_PATH} \
-#   --save_dir_name=${SAVE_DIR_NAME} \
-#   --k_shot=5 \
-#   --seeds_to_encode 42 43 44 45 46
+python3 -m spare.group_prompts \
+  --model_path=${MODEL_PATH} \
+  --save_dir_name=${SAVE_DIR_NAME} \
+  --k_shot=5 \
+  --seeds_to_encode 42 43 44 45 46
