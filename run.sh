80fa3c0cd6e6b3d65837d7c7e8de798ff7726176
giacomoarienti <arientigiacomo2003@gmail.com>
2025-03-22 21:37:06 +0100

    save_grouped_activations

--- run.sh
+++ run.sh
@@ -1,15 +1,3 @@
 pip install -e . 
 
-# python3 ./scripts/run_spare.py \
-#     --model_path="meta-llama/Meta-Llama-3-8B" \
-#     --data_name="nqswap" \
-#     --layer_ids 13 14 15 16 \
-#     --edit_degree=2.0 \
-#     --select_topk_proportion=0.07 \
-#     --seed=42 \
-#     --hiddens_name="grouped_activations" \
-#     --mutual_information_save_name="mutual_information" \
-#     --run_use_parameter \
-#     --run_use_context
-
-python3 demo.py meta-llama/Llama-2-7b-hf
+sh ./scripts/run_save_grouped_activations.sh "meta-llama/Llama-3.2-1B"
--- scripts/run_save_grouped_activations.sh
+++ scripts/run_save_grouped_activations.sh
@@ -5,15 +5,20 @@ set -u
 
 ulimit -n 10240
 
-export CUDA_VISIBLE_DEVICES=0
+if [ $# -lt 1 ]; then
+  echo "Error: Model path not provided."
+  echo "Usage: $0 <model_path>"
+  exit 1
+fi
 
+MODEL_PATH="${1}"
 
-python -m spare.save_grouped_activations \
+python3 -m spare.save_grouped_activations \
   --data_name="nqswap" \
   --model_path=${MODEL_PATH} \
   --load_data_name="grouped_prompts"\
-  --shots_to_encode 3 \
-  --seeds_to_encode 42 43 \
-  --save_hiddens_name="grouped_activations_3shot_seeds42-43"
+  --shots_to_encode 3 4 5 \
+  --seeds_to_encode 42 43 44 45 46 \
+  --save_hiddens_name="grouped_activations"
 
 wait
