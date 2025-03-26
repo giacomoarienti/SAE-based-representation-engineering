52b61ee4aa9e45e9508a618306008f5c400452c0
giacomoarienti <arientigiacomo2003@gmail.com>
2025-03-26 10:40:06 +0100

    new experiments

--- scripts/run_all_experiments.sh
+++ scripts/run_all_experiments.sh
@@ -87,3 +87,73 @@ for seed in $(seq 42 46); do
     --run_use_parameter \
     --run_use_context
 done
+
+for seed in $(seq 42 46); do
+  python3 ./scripts/run_spare.py \
+    --model_path="meta-llama/Llama-3.2-1B" \
+    --data_name="nqswap" \
+    --layer_ids 6 \
+    --edit_degree=2.0 \
+    --select_topk_proportion=0.07 \
+    --seed=${seed} \
+    --hiddens_name="grouped_activations" \
+    --mutual_information_save_name="mutual_information" \
+    --run_use_parameter \
+    --run_use_context
+done
+
+for seed in $(seq 42 46); do
+  python3 ./scripts/run_spare.py \
+    --model_path="meta-llama/Llama-3.2-1B" \
+    --data_name="nqswap" \
+    --layer_ids 7 \
+    --edit_degree=2.0 \
+    --select_topk_proportion=0.07 \
+    --seed=${seed} \
+    --hiddens_name="grouped_activations" \
+    --mutual_information_save_name="mutual_information" \
+    --run_use_parameter \
+    --run_use_context
+done
+
+for seed in $(seq 42 46); do
+  python3 ./scripts/run_spare.py \
+    --model_path="meta-llama/Llama-3.2-1B" \
+    --data_name="nqswap" \
+    --layer_ids 8 \
+    --edit_degree=2.0 \
+    --select_topk_proportion=0.07 \
+    --seed=${seed} \
+    --hiddens_name="grouped_activations" \
+    --mutual_information_save_name="mutual_information" \
+    --run_use_parameter \
+    --run_use_context
+done
+
+for seed in $(seq 42 46); do
+  python3 ./scripts/run_spare.py \
+    --model_path="meta-llama/Llama-3.2-1B" \
+    --data_name="nqswap" \
+    --layer_ids 9 \
+    --edit_degree=2.0 \
+    --select_topk_proportion=0.07 \
+    --seed=${seed} \
+    --hiddens_name="grouped_activations" \
+    --mutual_information_save_name="mutual_information" \
+    --run_use_parameter \
+    --run_use_context
+done
+
+for seed in $(seq 42 46); do
+  python3 ./scripts/run_spare.py \
+    --model_path="meta-llama/Llama-3.2-1B" \
+    --data_name="nqswap" \
+    --layer_ids 10 \
+    --edit_degree=2.0 \
+    --select_topk_proportion=0.07 \
+    --seed=${seed} \
+    --hiddens_name="grouped_activations" \
+    --mutual_information_save_name="mutual_information" \
+    --run_use_parameter \
+    --run_use_context
+done
