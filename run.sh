259ad3a155331ed44f46290e6638cd3e1416e9f5
giacomoarienti <arientigiacomo2003@gmail.com>
2025-04-14 18:42:52 +0200

    eval openbooks rog methods

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 MODEL_PATH="meta-llama/Llama-3.2-1B"
 DATASET="cwqswap"
-ROG="arrow"
+ROG="tuple"
 
 pip install -e . 
 
 # 1.
-# sh ./scripts/run_prepare_eval.sh "${MODEL_PATH}" "${DATASET}" "${ROG}"
+sh ./scripts/run_prepare_eval.sh "${MODEL_PATH}" "${DATASET}" "${ROG}"
 
 # 2.
 # sh ./scripts/run_memorised_set.sh "${MODEL_PATH}" "${DATASET}"
@@ -20,4 +20,4 @@ pip install -e .
 # sh ./scripts/run_mutual_information_and_expectation.sh "${MODEL_PATH}" "${DATASET}" "6 7 8 9 10 11 12"
 
 # 6.
-sh ./scripts/run_all_experiments.sh "${MODEL_PATH}" "${DATASET}"
+# sh ./scripts/run_all_experiments.sh "${MODEL_PATH}" "${DATASET}"
